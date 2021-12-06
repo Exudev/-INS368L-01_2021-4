a8229c4ba3124f92a4a7b4e5cde2ea11d0340589
exudes123 <53544279+exudes123@users.noreply.github.com>
2021-12-05 23:32:46 -0400

    Kata Doors

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Door
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public class Door
+        {
+            public bool IsOpen;
+            public bool Islocked;
+            public string key;
+          public bool Close(Door door)
+            {
+                return door.IsOpen = false;
+            }
+            
+           public  bool Open(Door door)
+            {
+                return door.IsOpen = true;
+            }
+          public   bool CheckStatus(Door door)
+            {
+                return door.IsOpen;
+            }
+           public bool Checklocked(Door door)
+            {
+                return door.Islocked;
+            }
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,60 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace TemperaturesTest
+namespace TestingDoor
 {
     [TestClass]
     public class UnitTest1
     {
-        //celsius
         [TestMethod]
-        public void CelsiusAKelvinTest()
+        public void TestOpen(Door.Program.Door door)
         {
-
-            double kelvinDado = Temperature.Program.CelsiusAKelvin(2);
-            double esperado =275.15;
-            Assert.AreEqual(esperado,kelvinDado);
+            bool reci = door.Open(door);
+            bool esperado = true;
+            Assert.AreEqual(esperado, reci);
         }
         [TestMethod]
-        public void CelsiusAfahrenheitTest()
+        public void TestClose(Door.Program.Door door)
         {
-
-            double KelvinDado = Temperature.Program.CelsiusAFahrenheit(15);
-            double esperado = 59;
-            Assert.AreEqual(esperado, KelvinDado);
+            bool reci = door.Open(door);
+            bool esperado = false;
+            Assert.AreEqual(esperado, reci);
         }
-        //kelvin
-        [TestMethod]
-        public void KelvinACelsiusTest()
-        {
-
-            double KelDado = Temperature.Program.KelvinACelsius(5);
-            double esperado = -268.15;
-            Assert.AreEqual(esperado, KelDado);
-        }
-        [TestMethod]
-        public void KelvinAfahrenheitTest()
-        {
-
-            double KelDado = Temperature.Program.KelvinAFahrenheit(10);
-            double esperado = -441.66999999999996;
-            Assert.AreEqual(esperado, KelDado);
-        }
-        //fahrenheit
-        [TestMethod]
-        public void fahrenheitAKelvin()
-        {
-
-            double fahDado = Temperature.Program.FahrenheitAKelvin(32);
-            double esperado = 273.15;
-            Assert.AreEqual(esperado, fahDado);
-        }
-        [TestMethod]
-        public void FahrenheitACelsiusTest()
-        {
-
-            double fahDado = Temperature.Program.FahrenheitACelsius(32);
-            double esperado = 0.0;
-            Assert.AreEqual(esperado, fahDado);
-        }
+       
     }
 }
