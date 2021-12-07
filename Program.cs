8694a851faf666b40cfe97eb68218796963ab0bc
exudes123 <53544279+exudes123@users.noreply.github.com>
2021-12-06 22:27:29 -0400

    Kata Vectors and Angles

--- Program.cs
+++ Program.cs
@@ -1,35 +1,45 @@
 ﻿using System;
 
-namespace Door
+namespace KataVectorsandAngles
 {
    public class Program
     {
         static void Main(string[] args)
         {
-            
+           
         }
-        public class Door
+
+        public class Vector
         {
-            public bool IsOpen;
-            public bool Islocked;
-            public string key;
-          public bool Close(Door door)
+           private int _posx;
+           private int _posy; 
+          public Vector(int posx, int posy)
             {
-                return door.IsOpen = false;
+                this._posx = posx;
+                this._posy = posy;
+
             }
+            public double Magnitude()
+            {
+                double result = Math.Sqrt(Math.Pow(this._posx, 2) + Math.Pow(this._posy,2));
+                return result;
+            }
+            public double DotProduct(Vector vector)
+            {
+                double result = (this._posx * vector._posx) + (this._posy * vector._posy);
+
+                return result;
             
-           public  bool Open(Door door)
-            {
-                return door.IsOpen = true;
             }
-          public   bool CheckStatus(Door door)
+            public double AngleBetween(Vector vector)
             {
-                return door.IsOpen;
+                double result = Math.Acos((DotProduct(vector)) / ((this.Magnitude()) * (vector.Magnitude())));
+                return result;
             }
-           public bool Checklocked(Door door)
-            {
-                return door.Islocked;
-            }
+
+
+
         }
+       
     }
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,24 +1,41 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace TestingDoor
+namespace TestVector
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestOpen(Door.Program.Door door)
-        {
-            bool reci = door.Open(door);
-            bool esperado = true;
-            Assert.AreEqual(esperado, reci);
-        }
-        [TestMethod]
-        public void TestClose(Door.Program.Door door)
-        {
-            bool reci = door.Open(door);
-            bool esperado = false;
-            Assert.AreEqual(esperado, reci);
-        }
        
+      
+        [TestMethod]
+        public void TestMagnitude()
+        {
+            KataVectorsandAngles.Program.Vector vector = new KataVectorsandAngles.Program.Vector(-2, 5);
+            double result = vector.Magnitude();
+            double esperado = System.Math.Sqrt(29);
+            Assert.AreEqual(esperado, result);
+        }
+        [TestMethod]
+        public void TestAngleBet()
+        {
+            KataVectorsandAngles.Program.Vector vector = new KataVectorsandAngles.Program.Vector(-2, 5);
+            KataVectorsandAngles.Program.Vector vector2 = new KataVectorsandAngles.Program.Vector(4, 3);
+            //recibido
+            double reci = vector2.AngleBetween(vector);
+            //esperado
+            double esperado = 1.3078015951139772;
+            //assert
+            Assert.AreEqual(esperado, reci);
+        }
+        [TestMethod]
+        public void TestDot()
+        {
+            KataVectorsandAngles.Program.Vector vector = new KataVectorsandAngles.Program.Vector(-2, 5);
+            KataVectorsandAngles.Program.Vector vector2 = new KataVectorsandAngles.Program.Vector(4, 3);
+            double result = vector.DotProduct(vector2);
+            int esperado = 7;
+            Assert.AreEqual(esperado, result);
+        }
+
     }
 }
